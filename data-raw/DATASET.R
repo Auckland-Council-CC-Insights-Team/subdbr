e97d03adc2f22ce76867bf7a5aee8d1372f4b0e4
mkacwork <mk.ting@aucklandcouncil.ac.nz>
2023-04-21 12:20:35 +1200

    updated file name references

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,8 +2,8 @@
 # csv_file <- readr::read_csv("data-raw/test_file_csv.csv")
 # txt_file <- readr::read_file("data-raw/test_file_txt.txt")
 
-excel_file <- "data-raw/test_file_xlsx"
-csv_file <- "data-raw/test_file_csv"
+excel_file <- "data-raw/test_file"
+csv_file <- "data-raw/test_file"
 
 usethis::use_data(excel_file
                   , csv_file
